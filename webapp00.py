c3f92ddd713d22ae50b4b72ee72eb21fabdefe18
SrCabeca <alysonoliveirasantos19@gmail.com>
2024-10-17 20:38:11 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -28,7 +28,7 @@ coluna1 = Colunas(3)
 with coluna1[0]:
   Escrever("")
 with coluna1[1]:
-  Escrever("ACTlib Versão 0.1", "titulo")
+  Escrever("FEEDBACK", "titulo")
   nome = Ler(rotulo = "Nome:", nmax=30, tipo="padrao", info="Inserção de Nome", autocompletar=None, na_mudanca=None, args=None, kwargs=None, placeholder="Não esqueça de preencher seu nome", desabilitada="falso", visibilidade="visivel")
   if nome:     
     Escrever("Seja Bem vinda(o), " + nome + "!")
